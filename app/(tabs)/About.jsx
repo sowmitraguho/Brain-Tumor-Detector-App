b0710c4b074725ba79c3f898cb3d98332746dfdd
Sowmitra Guha <sowmmochy@gmail.com>
2024-09-30 04:08:16 +0600

    about and explore part css done

--- app/(tabs)/About.jsx
+++ app/(tabs)/About.jsx
@@ -4,8 +4,19 @@ import React from 'react'
 export default function About() {
   return (
     <View>
-      <Text style={styles.boldLargeText}>About This App {'\n'}{'\n'} 
-      Welcome to the Brain Tumor Detection App, developed as part of my final year project at the University of Chittagong. My name is Sowmitra Guha, and I am a student in the Department of Electrical and Electronic Engineering. This innovative application utilizes a machine learning model to analyze MRI images for the early detection of brain tumors. By harnessing advanced algorithms, the app aims to provide accurate and efficient assessments, helping to facilitate timely medical intervention. Join me on this journey to leverage technology for improved healthcare outcomes!</Text>
+      <Text style={{
+          fontSize: 24,
+          fontWeight: 'bold',
+          color: '#4CAF50', // Green color for heading
+          marginBottom: 10,
+          marginHorizontal: 'auto'
+        }}>About This App </Text>
+      <Text style={{
+          fontSize: 16,
+          color: '#FF5722', // Deep orange color for points
+          margin: 15,
+          textAlign: 'justify'
+        }}>Welcome to the Brain Tumor Detection App, developed as part of my final year project at the University of Chittagong. My name is Sowmitra Guha, and I am a student in the Department of Electrical and Electronic Engineering. This innovative application utilizes a machine learning model to analyze MRI images for the early detection of brain tumors. By harnessing advanced algorithms, the app aims to provide accurate and efficient assessments, helping to facilitate timely medical intervention. Join me on this journey to leverage technology for improved healthcare outcomes!</Text>
     </View>
   )
 }
